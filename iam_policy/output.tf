09609d23e5e10d8bbeabe9c9958940eebf2c8c97
MrudhulaRani <mrudhula201095@gmail.com>
2019-05-22 12:29:29 +0530

    adding iam_policy module

---
+++ iam_policy/output.tf
@@ -0,0 +1,6 @@
+output "iam_policy_name" {
+  value = "${aws_iam_policy.iam_policy.name}"
+}
+output "iam_policy_id" {
+  value = "${aws_iam_policy.iam_policy.id}"
+}
---
+++ iam_policy/variables.tf
@@ -0,0 +1,19 @@
+variable "iam_policy_name" {}
+variable "iam_policy_path_url" {}
+variable "iam_policy_description" {}
+variable "iam_policy_document_JSON_formatted_string" {}
+# iam_policy_document_JSON_formatted_string =
+#policy = <<EOF
+#{
+# "Version": "2012-10-17",
+#"Statement": [
+#{
+#"Action": [
+#"ec2:Describe*"
+#],
+#"Effect": "Allow",
+#"Resource": "*"
+#}
+#]
+#}
+#EOF
