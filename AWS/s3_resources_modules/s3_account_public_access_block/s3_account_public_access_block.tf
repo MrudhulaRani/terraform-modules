9312ff282ac8e148b259f377aedf2de7af72c3df
MrudhulaRani <mrudhula201095@gmail.com>
2019-06-17 13:03:57 +0530

    adding s3 modules

--- AWS/s3_resources_modules/s3_account_public_access_block/s3_account_public_access_block.tf
+++ AWS/s3_resources_modules/s3_account_public_access_block/s3_account_public_access_block.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_account_public_access_block" "s3_account_public_access_block" {
   account_id = "${var.aws_account_id}"
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
+  block_public_acls = "${var.block_public_acls}"
+  block_public_policy = "${var.block_public_policy}"
+  ignore_public_acls = "${var.ignore_public_acls}"
+  restrict_public_buckets = "${var.restrict_public_buckets}"
 }
--- AWS/s3_resources_modules/s3_account_public_access_block/variables.tf
+++ AWS/s3_resources_modules/s3_account_public_access_block/variables.tf
@@ -1,1 +1,5 @@
 variable "aws_account_id" {}
+variable "block_public_acls" {}
+variable "block_public_policy" {}
+variable "ignore_public_acls" {}
+variable "restrict_public_buckets" {}
